f43b34c6b4eaa50b3b2cbbda0eff6eb7f1dfe6d5
WorthEx <7hecakeis4.l1e@gmail.com>
2024-05-30 17:04:56 +0500

    СПЕЦИАЛЬНЫЙ КОММИТ ДЛЯ VERCEL #3

--- src/components/pages/MainPage.vue
+++ src/components/pages/MainPage.vue
@@ -5,7 +5,10 @@ export default {
 </script>
 
 <template>
-  <div class="h-full w-full bg-slate-900">SOMETHING COOL</div>
+  <div class="absolute top-0 -z-[1000] h-full w-full bg-slate-900"></div>
+  <div class="grid h-full min-h-[90vh] w-full place-items-center text-white">
+    SOMETHING COOL
+  </div>
 </template>
 
 <style scoped></style>
