9d226c43b41e3481f73274c336cef45b37ae5907
2202zhan <zklzanov@gmail.com>
2024-05-31 19:56:58 +0500

    главная страничка

--- src/components/UI/BurgerNavLink.vue
+++ src/components/UI/BurgerNavLink.vue
@@ -4,13 +4,24 @@ export default {
     route: {
       required: true,
       type: String,
+    },
+    forId: {
+      type: Boolean,
+      default: false
     }
   }
 }
 </script>
 
 <template>
+  <a
+      v-if="forId"
+      :href="route"
+      class="text-[.45rem] py-3 px-4 font-medium uppercase active:bg-white/20 w-full h-full
+      hover:bg-black/30  active:backdrop-blur-lg animate-fade-down animate-once animate-duration-[600ms]">
+    <span class="font-light text-white"><slot></slot></span></a>
   <router-link
+      v-else
       :to="route"
       class="text-[.45rem] py-3 px-4 font-medium uppercase active:bg-white/20 w-full h-full
       hover:bg-black/30  active:backdrop-blur-lg animate-fade-down animate-once animate-duration-[600ms]">
--- src/components/UI/index.js
+++ src/components/UI/index.js
@@ -1,8 +1,9 @@
 import CoolButton from "@/components/UI/CoolButton.vue";
+import OrangeButton from '@/components/UI/OrangeButton.vue'
 import { library } from "@fortawesome/fontawesome-svg-core";
 import { faEnvelope, faLock } from "@fortawesome/free-solid-svg-icons";
 
 library.add(faLock);
 library.add(faEnvelope);
 
-export default [CoolButton];
+export default [CoolButton, OrangeButton];
