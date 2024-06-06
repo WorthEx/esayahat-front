67d4a11ce3bc724eaee6bbfd7e09f76ef5d77c83
WorthEx <7hecakeis4.l1e@gmail.com>
2024-06-06 22:06:52 +0500

    фикс для деплоя

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,8 +2,6 @@
 export default {
   content: [
     "./src/**/*.{html,js,vue}",
-    "node_modules/flowbite-vue/**/*.{js,jsx,ts,tsx,vue}",
-    "node_modules/flowbite/**/*.{js,jsx,ts,tsx}",
     // "./src/components/*.{html,js,vue}",
     // "./src/*.{html,js,vue}"
   ],
@@ -15,5 +13,5 @@ export default {
       sans: ["Roboto", "Audiowide"],
     },
   },
-  plugins: [require("tailwindcss-animated"), require("flowbite/plugin")],
+  plugins: [require("tailwindcss-animated")],
 }
