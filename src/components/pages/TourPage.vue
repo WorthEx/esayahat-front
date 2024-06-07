427e66536cb2edb52b83d8e897bb8a190658e359
WorthEx <7hecakeis4.l1e@gmail.com>
2024-06-07 15:51:57 +0500

    добавлена пустая страница одного тура

---
+++ src/components/pages/TourPage.vue
@@ -0,0 +1,22 @@
+<script>
+export default {
+  name: "TourPage",
+  computed: {
+    tourID() {
+      return this.$route.params.tourID
+    },
+  },
+}
+</script>
+
+<template>
+  <div class="absolute top-0 -z-[1000] h-full w-full bg-white"></div>
+  <div class="h-full min-h-[90vh] w-full pb-8 pt-0 ring md:pt-4">
+    <Container>
+      <div
+        class="relative flex h-full w-full flex-col gap-2 overflow-hidden rounded-lg bg-[#E4ECF4] p-2 sm:rounded-xl sm:p-3 lg:w-[800px] xl:w-[900px] 2xl:w-[1000px]"></div>
+    </Container>
+  </div>
+</template>
+
+<style scoped></style>
