87c53d736fb9294b2d447f5c1fe3282575506a2c
WorthEx <7hecakeis4.l1e@gmail.com>
2024-06-15 14:34:55 +0500

    minor fixes

--- src/components/UI/Container.vue
+++ src/components/UI/Container.vue
@@ -6,7 +6,7 @@ export default {
 
 <template>
   <div
-    class="mx-auto max-w-lg px-2 sm:px-4 lg:max-w-xl lg:px-0 xl:max-w-2xl 2xl:max-w-3xl">
+    class="relative mx-auto max-w-lg px-2 sm:px-4 lg:max-w-xl lg:px-0 xl:max-w-2xl 2xl:max-w-3xl">
     <slot></slot>
   </div>
 </template>
--- src/components/pages/NotFound.vue
+++ src/components/pages/NotFound.vue
@@ -6,11 +6,12 @@ export default {
 
 <template>
   <div class="absolute top-0 -z-[1000] h-full w-full bg-white"></div>
-  <div class="grid h-screen w-full place-items-center justify-items-center">
+  <div class="grid h-[calc(100vh-3em)] w-full place-items-center">
     <Container>
       <div
-        class="mb-28 flex animate-fade-up select-none flex-col items-center gap-2 text-center animate-delay-[300ms] animate-duration-[600ms] animate-once">
-        <span class="text-7xl font-semibold text-[#EF533F] sm:text-9xl"
+        class="flex animate-fade-up select-none flex-col items-center gap-2 text-center animate-delay-[300ms] animate-duration-[600ms] animate-once">
+        <span
+          class="animate-pulse text-7xl font-semibold text-[#EF533F] animate-duration-[2500ms] animate-infinite animate-ease-in sm:text-9xl"
           >404</span
         >
         <span class="text-xl"
@@ -19,7 +20,7 @@ export default {
         >
         <router-link class="w-fit text-sm" to="/"
           ><span
-            class="transition-all duration-300 ease-out after:mx-auto after:block after:h-[1.5px] after:w-0 after:bg-[#EF533F] after:transition-all after:duration-300 after:ease-out hover:text-[#EF533F] hover:after:w-full"
+            class="text-[#EF533F] transition-all duration-300 ease-out after:mx-auto after:block after:h-[1.5px] after:w-1/2 after:bg-[#EF533F] after:transition-all after:duration-300 after:ease-out hover:text-[#EF533F] hover:after:w-full"
             >Nvm take me home</span
           ></router-link
         >
